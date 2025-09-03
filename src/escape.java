10e80eb4c16880e6fdf64a079cd87e1bf091c2aa
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-03 20:01:12 +0530

    Added FirstProgram.java

---
+++ src/escape.java
@@ -0,0 +1,10 @@
+public class escape {
+    public static void main(String[] args) {
+        System.out.println("Hello 'DVCoding'...");
+        System.out.println("Hello \"DVCoding\"...");
+        System.out.println("Hello \\DVCoding\\...");
+        System.out.println("Hello \n DVCoding...");
+        System.out.println("Hello \t DVCoding...");
+        System.out.println("Hello\bDVCoding...");
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,24 @@
+public class variables {
+    public static void main(String [] args){
+        int myNumber = 865;
+        int yourNumber;
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+
+        double myDouble = 6.78647;
+        System.out.println(myDouble);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+        char myCharacter = 'K';
+        System.out.println(myCharacter);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+    }
+}
