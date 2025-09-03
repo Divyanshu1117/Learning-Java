2f6a53fac9fa7e0139ad5aecafa1b448a0e12a1b
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-03 23:36:54 +0530

    Add new programs

--- src/Escape.java
+++ src/Escape.java
@@ -1,4 +1,4 @@
-public class escape {
+public class Escape {
     public static void main(String[] args) {
         System.out.println("Hello 'DVCoding'...");
         System.out.println("Hello \"DVCoding\"...");
--- src/Subscribe.java
+++ src/Subscribe.java
@@ -1,4 +1,4 @@
-public class subscribe {
+public class Subscribe {
     public static void main(String [] args){
         System.out.println("Subscribe");
     }
