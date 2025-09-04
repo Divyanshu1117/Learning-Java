5fc7d9ed33bd5261f33317a2d2fc535c7d8e8961
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-04 20:32:23 +0530

    Add new programs

--- src/J17_Assignment.java
+++
@@ -1,5 +0,0 @@
-public class J17_Assignment {
-    public static void main(String [] args){
-        System.out.println("Good Morning");
-    }
-}
---
+++ src/J27_Q13.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class J27_Q13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Temperature converter: ");
+        System.out.print("Enter your temp in F: ");
+        float fah = input.nextFloat();
+        float cel = (fah - 32) * 5.0f / 9.0f;
+        System.out.println("Your temperature is: " + cel + "C");
+    }
+}
