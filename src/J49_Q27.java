2a931b0a7445c41f6db9b0d272bcb97526bc6b8b
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-11 17:21:27 +0530

    Add new files

---
+++ src/J49_Q27.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class J49_Q27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Multiplication World\n");
+        System.out.print("Please Enter Your Number: ");
+        int num = input.nextInt();
+        printMultiplicationTable(num);
+    }
+
+    public static void printMultiplicationTable(int num) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(num + " X " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
