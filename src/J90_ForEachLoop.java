699de9648e3b3e68d461c71310bbf40e2d236d6c
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-21 00:40:46 +0530

    Add new programs

---
+++ src/J90_ForEachLoop.java
@@ -0,0 +1,18 @@
+public class J90_ForEachLoop {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Ram", "Shyam", "Mohan", "Sohan", "Sita", "Geeta"};
+//        printArray(names);
+        printArrayForEach(names);
+    }
+
+    //    public static void printArray(String[] names) {
+//        for (int i = 0; i < names.length; i++) {
+//            System.out.println(names[i]);
+//        }
+//    }
+    static void printArrayForEach(String[] names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
