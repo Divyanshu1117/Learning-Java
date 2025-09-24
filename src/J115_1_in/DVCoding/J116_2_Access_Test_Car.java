b89c9fad9a0e2a1e83889335824ed9c02895a566
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-09-24 20:32:17 +0530

    Programs Updated

--- src/J115_1_in/DVCoding/J116_2_Access_Test_Car.java
+++ src/J115_1_in/DVCoding/J116_2_Access_Test_Car.java
@@ -9,6 +9,6 @@ public class J116_2_Access_Test_Car {
         car.costOfPurchase = 100000;
         J116_1_Car_Public newCar = new J116_1_Car_Public("Black", "Maruti", 1, 70000);
         System.out.println(newCar);
-        J117_Default def = new J117_Default();
+        J116_3_Default def = new J116_3_Default();
     }
 }
---
+++ src/J115_2_in/DivyanshuCoding/J116_4_Default_Test.java
@@ -0,0 +1,12 @@
+package J115_2_in.DivyanshuCoding;
+
+import J115_1_in.DVCoding.J116_1_Car_Public;
+
+public class J116_4_Default_Test {
+    public static void main(String[] args) {
+        J116_1_Car_Public car = new J116_1_Car_Public("Yellow", "ZEN", 5, 30000);
+        car.model = "EON";
+//        car.costOfPurchase = 80000;
+        J115_1_in.DVCoding.J116_1_Car_Public def;
+    }
+}
