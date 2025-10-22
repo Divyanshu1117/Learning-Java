1ead09707fe262ea235fcbdc9d1fa0a11bf04d52
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-10-22 18:14:05 +0530

    Add new programs

---
+++ src/J140_Testing_Wrapper/Testing_Wrapper.java
@@ -0,0 +1,9 @@
+package J140_Testing_Wrapper;
+
+public class Testing_Wrapper {
+    public static void main(String[] args) {
+        Integer first = 55;
+        System.out.println(first);
+        int second = first;
+    }
+}
