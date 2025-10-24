17052fb6df4f5dec1476278a5a23a20a4cbabcef
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-10-24 23:23:40 +0530

    Add new programs

---
+++ src/J145_Q89/VarArgs.java
@@ -0,0 +1,17 @@
+package J145_Q89;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        System.out.println(concatenate("Divyanshu"));
+        System.out.println(concatenate("Lovish", "Kamboj"));
+        System.out.println(concatenate("My", "name", "is", "Divyanshu"));
+    }
+
+    public static String concatenate(String... strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str).append(" ");
+        }
+        return sb.toString();
+    }
+}
